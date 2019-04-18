4c196cb45b87f57483ce74f8e973b986db6e2d0e
Claudio Cardoso <claudio.cardoso.89@gmail.com>
2019-04-18 02:09:45 +0100

    persistence

--- login-script.js
+++ login-script.js
@@ -1,80 +1,89 @@
 $(document).ready(function () {
 
-
-    function User(fullname, username, password, email) {
-        this.getFullname = function () {
-            return fullname;
-        }
-        this.getUsername = function () {
-            return username;
-        }
-        this.getPassword = function () {
-            return password;
-        }
-        this.getEmail = function () {
-            return email;
-        }
-    }
-
-    var andre = new User("André Lança", "andrelanca14", "asd123", "andrelanca14@gmail.com");
-    var pedro = new User("Pedro Campos", "pcampos", "pcampos", "pcampos@gmail.com");
-    var ana = new User("Ana Serafim", "serafim19", "ser12", "anaserafim@gmail.com");
-    var fabio = new User("Fábio Sequeira", "fabiolous", "fbio", "fabio@gmail.com");
-
-    var arrOfUser = [andre, pedro, ana, fabio];
-
-
-
-    /***********
-     * Login
-     */
-
-    $("#login-button").click(function (event) {
-        var usernameInput = $("#username").val();
-        var passwordInput = $("#password").val();
-
-        var isLogged = arrOfUser.some(function (element) {
-            return element.getUsername() === usernameInput && element.getPassword() === passwordInput;
-        })
-
-        if (isLogged) {
-            window.location.href = "register.html";
-        }
-
-    });
-    /***********
-     * Eye
-     */
-
-    var eye = $("#eye");
-
-    eye.click(function () {
-        var pass = $("#password");
-        
-        if (pass.prop('type', 'password')) {
-            pass.prop('type', 'text');
-
-        } 
-    });
-
-    /***********
-     * Register
-     ***********/
-
-    $("#next-btn").click(function (event) {
-
-        arrOfUser.push(new User($("#nameInput").val(), $("#userInput").val(), $("#passInput").val(), $("#emailInput").val()));
-        console.log(arrOfUser);
- 
-    })
-     
-
-
-
-
-
-
-
-
+    $("#next-btn").click(addUser);
+    console.log("Doc ready");
+    var arrOfUser;
+    getUsers();
 
 });
+
+function getUsers() {
+    $.ajax({
+        url: "http://localhost:8080/beastme/",
+        async: true,
+        success: logUsers,
+        error: errorCallback
+    });
+}
+
+function logUsers(response) {
+    console.log(response);
+    arrOfUser = response;
+}
+
+function errorCallback(request) {
+    console.log(request);
+}
+
+
+/***********
+ * Login
+ */
+
+$("#login-button").click(function (event) {
+    var usernameInput = $("#username").val();
+    var passwordInput = $("#password").val();
+
+    var isLogged = arrOfUser.some(function (element) {
+        return element.getUsername() === usernameInput && element.getPassword() === passwordInput;
+    })
+
+    if (isLogged) {
+        startChat(usernameInput);
+    }
+
+});
+/***********
+ * Eye
+ */
+
+var eye = $("#eye");
+
+eye.click(function () {
+    var pass = $("#password");
+
+    if (pass.prop('type', 'password')) {
+        pass.prop('type', 'text');
+
+    }
+});
+
+/***********
+ * Register
+ ***********/
+
+function addUser(event) {
+    console.log("trying to add")
+    $.ajax({
+        url: 'http://localhost:8080/beastme/',
+        type: 'POST',
+        data: JSON.stringify({
+            name: $("#nameInput").val(),
+            username: $("#userInput").val(),
+            password: $("#passInput").val(),
+            email: $("#emailInput").val()
+        }),
+        async: true,
+        contentType: 'application/json',
+        success: goToQuestions,
+        error: errorCallback
+    });
+
+}
+
+
+function goToQuestions(response) {
+    console.log("questions")
+    getUsers();
+}
+
